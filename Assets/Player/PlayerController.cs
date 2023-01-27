2f7c7ca01706cec18c6902447270fe548fb2e487
DanyeWestinator <44176712+DanyeWestinator@users.noreply.github.com>
2023-01-27 14:54:00 -0800

    basic player input

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>
+    /// How fast the player moves
+    /// </summary>
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    public Vector3 display;
+
+    /// <summary>
+    /// The direction the player is moving
+    /// </summary>
+    private Vector2 dir;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    
+    /// <summary>
+    /// Update the player's move direction based on input
+    /// </summary>
+    /// <param name="value">The input value for movement</param>
+    void OnMove(InputValue value)
+    {
+        //The current stick direction
+        dir = value.Get<Vector2>();
+        //Ignore small values from stick drift
+        if (dir.magnitude <= 0.1f)
+            dir = Vector2.zero;
+    }
+    
+    /// <summary>
+    /// Handles move logic
+    /// </summary>
+    void Move()
+    {
+        Vector3 direction = (Vector3)dir * Time.deltaTime * moveSpeed;
+        display = direction;
+
+        transform.position += direction;
     }
 }
