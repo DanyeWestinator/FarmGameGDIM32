3c58f484517da528f2958344268dfabb05ef989f
Daichi <daichi@murokami.com>
2023-02-21 11:21:26 -0800

    merged main:

---
+++ Assets/Farm/FarmBase.cs
@@ -0,0 +1,22 @@
+/// <summary>
+/// a farm plot, stoes farm tile objects by position and returns them when asked
+/// </summary>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
